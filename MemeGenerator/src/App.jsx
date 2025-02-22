c848d8e04b5a5a6bbc44dfdfe9203a94d5c2db7b
HlapecMihad <aljaz.kodric03@gmail.com>
2025-02-22 11:43:50 +0100

    initialization of vite react

---
+++ MemeGenerator/src/App.jsx
@@ -0,0 +1,16 @@
+import './index.css'
+import Main from './components/Main'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+      <h1>Meme Generator</h1>
+      <Main />
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ MemeGenerator/src/components/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main() {
+    return (
+        <>
+            <h1>I am main</h1>
+        </>
+    )
+}
